1483ece9d1557c1dff158fadb8d4d9324e3ac68a
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-09 14:30:52 +0530

    the door can only be opened if the player has the key

---
+++ static_body_2d.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+func _ready() -> void:
+	pass 
+	
+func _process(delta: float) -> void:
+	pass
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player_group"):
+		if body.has_key==true:
+			open_the_door()
+func open_the_door():
+	$WallCollision.set_deferred("disabled",true)
+	$Door_image/StaticBody2D/CollisionPolygon2D.set_deferred("disabled",true)
+	hide()
