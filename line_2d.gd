0d662c0d9ac51bc64399b4336031ee4fc8a73bff
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-10 14:25:29 +0530

    added 2 more doors and resolve the collision glitch

--- line_2d.gd
+++ line_2d.gd
@@ -1,22 +1,15 @@
 extends Line2D
 
-# This ensures the node is found before _ready() runs
-@onready var occluder_node = $LightOccluder2D 
+@onready var occluder_node = $LightOccluder2D
+@onready var collision_poly = $StaticBody2D/CollisionPolygon2D
 
 func _ready():
-	# 1. Check if the node actually exists to prevent a crash
-	if not occluder_node:
-		print("Error: LightOccluder2D not found! Check the Scene Tree.")
-		return
-
-	var occluder_poly = OccluderPolygon2D.new()
-	var collision_poly = $StaticBody2D/CollisionPolygon2D
-	var shadow_shape = Geometry2D.offset_polyline(points, width / 2, Geometry2D.JOIN_ROUND, Geometry2D.END_ROUND)
-	var points_for_collision = Geometry2D.offset_polyline(points, width / 2, Geometry2D.JOIN_ROUND, Geometry2D.END_ROUND)
-
-	if shadow_shape.size() > 0:
-		occluder_poly.polygon = shadow_shape[0]
+	var shapes = Geometry2D.offset_polyline(points, width / 2, Geometry2D.JOIN_ROUND, Geometry2D.END_ROUND)
+	
+	if shapes.size() > 0:
+		var poly_data = shapes[0]
+		
+		var occluder_poly = OccluderPolygon2D.new()
+		occluder_poly.polygon = poly_data
 		occluder_node.occluder = occluder_poly
-
-	if points_for_collision.size() > 0:
-		collision_poly.polygon = points_for_collision[0]
+		collision_poly.polygon = poly_data
