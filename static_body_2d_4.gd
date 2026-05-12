a4bbb1ea453f7897729840266322e525a006e003
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-12 13:41:33 +0530

    added progress bar that shows time left to insert (for some reason it isnt visible i'm figuring out

--- static_body_2d_4.gd
+++ static_body_2d_4.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 @onready var TempLabel: Label = $"../player/Camera2D/CanvasLayer/TempLabel"
 @onready var TimerLabel: Label = $"../player/Camera2D/CanvasLayer/TimerLabel"
 @onready var timer: Timer = $Timer
+@onready var texture_progress_bar: TextureProgressBar = $"../player/Camera2D/CanvasLayer/TextureProgressBar"
 
 
 #variables
@@ -25,17 +26,24 @@ func _process(delta: float) -> void:
 	else:
 		TimerLabel.text="REACTION STABILIZED"
 		
+	if timer.time_left>0:	
+		texture_progress_bar.visible=true
+		texture_progress_bar.value= (timer.wait_time - timer.time_left)/timer.wait_time * 100
+	else:
+		texture_progress_bar.visible=false
+
 
 func _on_socket_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player_group") and body.has_boron_rod:
 		player_ref=body
 		timer.start()
-
+	
 
 func _on_socket_zone_body_exited(body: Node2D) -> void:
 	if body == player_ref:
 		print("Interrupted!")
 		timer.stop()
+		texture_progress_bar.visible=false
 		player_ref=null
 
 func _on_timer_timeout() -> void:
@@ -46,4 +54,5 @@ func insert_rod(player):
 	rods_inserted+=1
 	player.has_boron_rod=false
 	player_ref=null
+	texture_progress_bar.visible=false
 	print("Control rod slot: ",rods_inserted, " Locked.")
