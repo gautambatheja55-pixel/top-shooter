f241295eae938f1b6a32d2b78a16b1d77714cbb3
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-17 14:20:40 +0530

    added enemy to find player usuing raycast

--- enemy.gd
+++ enemy.gd
@@ -1,14 +1,49 @@
 extends CharacterBody2D
 
-var motion = Vector2()
+enum State {IDLE, SPOTTED, CHASING}
+var current_state = State.IDLE
+var alert_timer = 0.4
+@onready var ray: RayCast2D = $RayCast2D
+@onready var player: CharacterBody2D = $"../player"
+@onready var danger_glow: Panel = $"../player/Camera2D/CanvasLayer/DangerGlow"
+var speed = 150
 
-func _physics_process(delta: float) -> void:
-	var Player = get_parent().get_node("player")
-	
-	position += (Player.position - position)/50
-	look_at(Player.position)
-	move_and_collide(motion)
+func _physics_process(delta: float):
+	if player and ray:
+		ray.target_position = to_local(player.global_position)
+		
+		var can_see_player = false
+		if ray.is_colliding():
+			var thing_we_hit = ray.get_collider()
+			if thing_we_hit == player:
+				can_see_player = true
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("bullet"):
-		queue_free()
+		match current_state:
+			State.IDLE:
+				var distance = global_position.distance_to(player.global_position)
+				if distance < 450.0 and can_see_player:
+					current_state = State.SPOTTED
+					alert_timer = 0.4
+					if danger_glow:
+						danger_glow.visible = true
+						
+			State.SPOTTED:
+				velocity = Vector2.ZERO
+				alert_timer -= delta
+				look_at(player.global_position)
+				if alert_timer <= 0.0:
+					current_state = State.CHASING
+					
+			State.CHASING:
+				look_at(player.global_position)
+				var direction = (player.global_position - global_position).normalized()
+				velocity = direction * speed
+				
+				move_and_slide()
+				
+				var distance_to_player = global_position.distance_to(player.global_position)
+				if distance_to_player < 25.0:
+					get_tree().reload_current_scene()
+
+		for i in get_slide_collision_count():
+			pass
