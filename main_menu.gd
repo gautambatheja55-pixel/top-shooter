e5d169b7114130ef8bed903c5997ee2589cda0ad
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-21 15:53:31 +0530

    designed the main menu

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+@onready var button: Button = $Button
+@onready var button_2: Button = $Button2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world.tscn")
