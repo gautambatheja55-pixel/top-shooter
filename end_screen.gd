abad7901755028a2c5f74efc7e0d71f1016a730e
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-25 12:32:48 +0530

    added a new sound effect and removed unnecessarly photos and audio that are no longer needed

--- end_screen.gd
+++ end_screen.gd
@@ -28,11 +28,9 @@ func _on_vedio_end_timer_timeout() -> void:
 func _on_type_timer_timeout() -> void:
 	if current_character<full_length.length():
 		typing_label.text+=full_length[current_character]
+		typing.play()
 		current_character+=1
-		if not typing.playing:
-			typing.play()
 		
-
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
