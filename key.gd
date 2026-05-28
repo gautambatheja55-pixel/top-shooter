d3e224ebf95ee3930bfbdc01882720ea0f3894fd
Gautam Batheja <gautambatheja55@gmail.com>
2026-05-28 12:09:08 +0530

    completed the game!

--- key.gd
+++ key.gd
@@ -1,6 +1,9 @@
 extends Area2D
+@onready var pickup: AudioStreamPlayer2D = $Pickup
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player_group"):
 		body.has_key = true
-		queue_free()
+		pickup.play()
+		$CollisionShape2D.set_deferred("disabled",true)
+		hide()
